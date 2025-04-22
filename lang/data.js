e47f5f61b486b6c4b1a7a988ffab9e82950bb348
kurtis <xk233qwq@163.com>
2025-04-22 15:47:04 +0800

    类型、值和变量

--- lang/data.js
+++ lang/data.js
@@ -3,22 +3,72 @@
 let v // 声明变量
 let _v = 1 // 声明并赋值
 let _v1 = 'a', _v2 = 1
-_v3 = 1 // 也可以不声明直接定义
 
-v = 1               // 整数数值
-v = 0.01;           // 实数数值
-v = "hello world"   // 字符串
-v = 'JavaScript'    // 单引号声明字符串
-v = true            // 布尔值真
-v = false           // 布尔值假
-v = null            // 空值
-v = undefined       // 未定义值
+const c = null // 声明常量，常量必须在声明时赋值
+
+// let 和 const 都受作用域限制
+{
+    let l = 1
+    console.log(l)
+}
+// console.log(l) // ReferenceError
+
+// 使用 var 声明变量不受作用域限制，全局可用
+{
+    var l = 5
+    glo = 10 // 不使用关键字声明的变量默认为 var 变量
+}
+console.log(l)
+console.log(glo)
+
+v = 1                           // 整数数值
+v = 0x10                        // 十六进制字面量
+v = 0o10                        // 八进制字面量
+v = 0b10                        // 二进制字面量
+v = Infinity                    // 正无穷
+v = Number.POSITIVE_INFINITY    // 同上
+v = -Infinity                   // 负无穷
+v = Number.NEGATIVE_INFINITY    // 同上
+v = NaN                         // 非数值
+v = Number.NaN                  // 同上
+v = 123n                        // BigInt 字面量
+v = 0.01;                       // 实数数值
+v = 1.01E-2                     // 科学计数法
+v = "hello world"               // 字符串
+v = 'JavaScript'                // 单引号声明字符串
+v = `JavaScript`                // 反引号声明字符串
+v = true                        // 布尔值真
+v = false                       // 布尔值假
+v = null                        // 空值
+v = undefined                   // 未定义值
+v = Symbol()                    // 符号
+
+// 续行，实际只有一行
+v = 'hello \
+world'
+
+// 反引号可以声明多行字符串
+v = `hello 
+world`
+
+// 反引号可以使用插值
+i = 1
+v = `value: ${i}`
+
+// 如果反引号声明的字符串前面有一个函数名，这个字符串会作为参数传入
+console.log(String.raw`\n`.length)
+
+// 正则表达式
+v = /^\d\w$/
 
 // 对象
-v = { name: "JavaScript", }
+v = { name: 'JavaScript', }
+
+// 日期时间
+now = Date.now()
 
 v.name              // 访问对象属性
-v["name"]           // 另一种访问方式
+v['name']           // 另一种访问方式
 v = {}              // 空对象
 
 // 条件访问
@@ -29,3 +79,17 @@ v = [1, 2, 3]
 v[0]                // 索引访问
 v.length            // 数组长度
 v = []              // 空数组，length 值为 0
+
+// 解构
+v = { name: 'JavaScript', type: 'dynamic' }
+let { name, type, test } = v
+console.log(`name: ${name}, type: ${type}, test: ${test})`)
+let { name: n, type: t }  = v
+console.log(`name: ${n}, type: ${t}`)
+v = [132, 456]
+let [x, y, z] = v
+console.log(`x: ${x}, y: ${y}, z: ${z}`)
+
+v = [1, 2, 3, 4]
+let [x1, ...y1] = v
+console.log(`x = ${x1}, y: ${y1}`)
